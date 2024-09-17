5ccd60810669deb5833b2eb976f1e6ec08de6a1e
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-09-17 09:56:48 +0600

    dictsort filter

--- Module 10.5 Practice/sample_project/sample_app/views.py
+++ Module 10.5 Practice/sample_project/sample_app/views.py
@@ -6,6 +6,13 @@ def home(request):
          'date' : datetime.datetime.now(),
          'val' : '', 
          'valu' : '20',
-         'nm' : 'joyant'
+         'nm' : 'joyant',
+         'string' : 'Python Is Fun',
+         'details' : [
+            {'name': 'Josh', 'age': 19},
+            {'name': 'Dave', 'age': 22},
+            {'name': 'Joe', 'age': 31},
+         ],
+         
          }
     return render(request, 'home.html', f)
