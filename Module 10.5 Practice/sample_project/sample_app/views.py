84c59f5c5f6ef9679cd1fd4e9b293ed8752085aa
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-09-17 09:13:52 +0600

    date filter

--- Module 10.5 Practice/sample_project/sample_app/views.py
+++ Module 10.5 Practice/sample_project/sample_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 def home(request):
-    f = {'name' : ['Joyant', 'Sheikhar', 'Gupta', 'Joy']}
+    f = {'name' : ['Joyant', 'Sheikhar', 'Gupta', 'Joy'], 'date' : datetime.datetime.now()}
     return render(request, 'home.html', f)
