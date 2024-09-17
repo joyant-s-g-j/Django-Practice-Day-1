cbfa8c449b30a74def237ec961cf108f7025f052
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-09-17 09:21:41 +0600

    length filter

--- Module 10.5 Practice/sample_project/sample_app/views.py
+++ Module 10.5 Practice/sample_project/sample_app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 import datetime
 # Create your views here.
 def home(request):
-    f = {'name' : ['Joyant', 'Sheikhar', 'Gupta', 'Joy'], 'date' : datetime.datetime.now()}
+    f = {'name' : ['Joyant', 'Sheikhar', 'Gupta', 'Joy'], 'date' : datetime.datetime.now(),
+         'val' : '',
+         }
     return render(request, 'home.html', f)
