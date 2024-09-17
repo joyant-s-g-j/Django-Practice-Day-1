c5785647b7008354c833dbba5012125a4ca13e24
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-09-17 09:43:10 +0600

    Capfirst Filter

--- Module 10.5 Practice/sample_project/sample_app/views.py
+++ Module 10.5 Practice/sample_project/sample_app/views.py
@@ -6,6 +6,6 @@ def home(request):
          'date' : datetime.datetime.now(),
          'val' : '', 
          'valu' : '20',
-         
+         'nm' : 'joyant'
          }
     return render(request, 'home.html', f)
